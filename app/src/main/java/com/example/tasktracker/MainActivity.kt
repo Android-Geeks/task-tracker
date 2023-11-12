7295804bfb29d2f7415c4ad24f49a24b9efef3bb
Ali <alialashwall14@gmail.com>
2023-11-12 20:43:48 +0200

    login and register screens

--- app/src/main/java/com/example/tasktracker/MainActivity.kt
+++ app/src/main/java/com/example/tasktracker/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tasktracker.ui.theme.TaskTrackerTheme
+import com.example.tasktracker.ui.theme.screens.RegisterScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val todoViewModel : TodoViewModel = viewModel()
+                    RegisterScreen(todoViewModel)
                 }
             }
         }
