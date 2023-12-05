9684aa7bc60cd2a10b9ae6373ed09742598078b4
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-12-05 22:22:33 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -86,7 +86,7 @@ dependencies {
 
     //Retrofit
     val retrofit = "2.9.0"
-    val okHttp = "4.11.0"
+    val okHttp = "4.12.0"
     implementation("com.squareup.retrofit2:retrofit:${retrofit}")
     implementation("com.squareup.retrofit2:converter-gson:${retrofit}")
     implementation("com.squareup.okhttp3:okhttp:${okHttp}")
