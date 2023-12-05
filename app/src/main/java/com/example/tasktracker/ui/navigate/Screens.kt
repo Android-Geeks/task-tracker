b000df6ba21286bdd76a1167fc1e2083be5e8648
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-12-06 01:52:45 +0200

    change navigation screens

--- app/src/main/java/com/example/tasktracker/ui/navigate/Screens.kt
+++ app/src/main/java/com/example/tasktracker/ui/navigate/Screens.kt
@@ -5,10 +5,10 @@ import androidx.annotation.StringRes
 import com.example.tasktracker.R
 
 sealed class Screen(val route: String) {
-    object OnBoard : Screen("onboard")
-    object Start : Screen("start")
-    object Login : Screen("login")
-    object Register : Screen("register")
+    data object OnBoard : Screen("onboard")
+    data object Start : Screen("start")
+    data object Login : Screen("login")
+    data object Register : Screen("register")
 }
 
 sealed class BottomNavScreen(
@@ -16,8 +16,8 @@ sealed class BottomNavScreen(
     @StringRes val resourceId: Int,
     @DrawableRes val iconId: Int
 ) {
-    object Home : BottomNavScreen("home", R.string.home, R.drawable.home)
-    object Calender : BottomNavScreen("calender", R.string.calender, R.drawable.calendar)
-    object Profile : BottomNavScreen("profile", R.string.profile, R.drawable.profile)
-    object Settings : BottomNavScreen("settings", R.string.settings, R.drawable.settings)
+    data object Home : BottomNavScreen("home", R.string.home, R.drawable.home)
+    data object Calender : BottomNavScreen("calender", R.string.calender, R.drawable.calendar)
+    data object Profile : BottomNavScreen("profile", R.string.profile, R.drawable.profile)
+    data object Settings : BottomNavScreen("settings", R.string.settings, R.drawable.settings)
 }
